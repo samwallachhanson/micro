f4433c68dddf21779d55ab7950f71b1d983c488c
samwallachhanson <sam.wallach.hanson@gmail.com>
2021-01-26 19:24:11 +0000

    333

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210126164051) do
+ActiveRecord::Schema.define(version: 20210126191519) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
